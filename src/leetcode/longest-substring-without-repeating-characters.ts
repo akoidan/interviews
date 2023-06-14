d74f811ffb8c2c77428c943eb8171cb2944bf2ef
andrew <andrew.koidan@vertrical.com>
2023-06-14 11:27:13 +0300

    adde letcode

---
+++ src/leetcode/longest-substring-without-repeating-characters.ts
@@ -0,0 +1,49 @@
+/*
+ Given a string s, find the length of the longest
+ substring
+ without repeating characters.
+
+ https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+ Example 1:
+
+ Input: s = "abcabcbb"
+ Output: 3
+ Explanation: The answer is "abc", with the length of 3.
+ Example 2:
+
+ Input: s = "bbbbb"
+ Output: 1
+ Explanation: The answer is "b", with the length of 1.
+ Example 3:
+
+ Input: s = "pwwkew"
+ Output: 3
+ Explanation: The answer is "wke", with the length of 3.
+ Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+ Constraints:
+
+ 0 <= s.length <= 5 * 104
+ s consists of English letters, digits, symbols and spaces.
+ */
+
+export function lengthOfLongestSubstring(s: string): number {
+
+  let set = new Map();
+  let biggestAmount: number = 0;
+  let i = 0;
+  while (i < s.length) {
+    let current = s[i];
+    if (set.has(current)) {
+      biggestAmount = Math.max(set.size, biggestAmount);
+      i = set.get(current)
+      set.clear()
+    } else {
+      set.set(current, i)
+    }
+    i ++;
+  }
+  return Math.max(set.size, biggestAmount);
+};
