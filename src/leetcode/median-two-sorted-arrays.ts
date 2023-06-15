43762d4705384ba434e4a4cacff0ce3d9a9c21fd
andrew <andrew.koidan@vertrical.com>
2023-06-16 00:32:28 +0300

    -5 hours of my life

--- src/leetcode/median-two-sorted-arrays.ts
+++ src/leetcode/median-two-sorted-arrays.ts
@@ -1,21 +1,70 @@
-export function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-  // TODO remove 1 from indexes
-  let num1Index = -1;
-  let num2Index = -1;
-  let medianStartsFrom = Math.floor((nums1.length + nums2.length - 1) / 2)
+/*
+ Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
-  while ((num1Index + num2Index) < medianStartsFrom - 2) {
-    if (num1Index < nums1.length - 1 && nums1[num1Index + 1] < nums2[num2Index + 1]) {
-      num1Index++;
-    } else {
-      num2Index++;
-    }
+ The overall run time complexity should be O(log (m+n)).
+
+ Example 1:
+ Input: nums1 = [1,3], nums2 = [2]
+ Output: 2.00000
+ Explanation: merged array = [1,2,3] and median is 2.
+
+ Example 2:
+ Input: nums1 = [1,2], nums2 = [3,4]
+ Output: 2.50000
+ Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
+ Constraints:
+
+ nums1.length == m
+ nums2.length == n
+ 0 <= m <= 1000
+ 0 <= n <= 1000
+ 1 <= m + n <= 2000
+ -10^6 <= nums1[i], nums2[i] <= 10^6
+
+ */
+
+class TwoArrayIterator {
+  i1: number = 0;
+  i2: number = 0;
+
+  constructor(private nums1: number[], private nums2: number[]) {
   }
 
-  // [1,3,7,9], [2, 4, 5] -> 1,2,3,   4    ,5,7,9
-  if (nums1[num1Index + 1] > nums2[num2Index + 1]) {
-    return nums2[num2Index +1]
+  getNext(): number {
+    let c;
+    if (this.i2 >= this.nums2.length) {
+      c = this.nums1[this.i1];
+      this.i1++
+    } else if (this.i1 >= this.nums1.length) {
+      c = this.nums2[this.i2];
+      this.i2++
+    } else if (this.nums1[this.i1] < this.nums2[this.i2]) {
+      c = this.nums1[this.i1];
+      this.i1++
+    } else {
+      c = this.nums2[this.i2];
+      this.i2++
+    }
+    return c;
+  }
+
+  get currentIndex() {
+    return this.i1 + this.i2;
+  }
+}
+
+
+export function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+  let iterator = new TwoArrayIterator(nums1, nums2)
+  let medianStartsFrom = Math.floor((nums1.length + nums2.length - 1) / 2)
+  while (iterator.currentIndex < medianStartsFrom) {
+    iterator.getNext()
+  }
+  if ((nums2.length + nums1.length ) % 2 == 1) {
+    return iterator.getNext();
   } else {
-    return nums1[num1Index +1]
+    return (iterator.getNext() + iterator.getNext()) / 2;
   }
 }
--- test/median-two-sorted-arrays.ts
+++ test/median-two-sorted-arrays.ts
@@ -8,7 +8,7 @@ describe("median-two-sorted-arrays", () => {
   });
   it("nums1 = [1,3,7,9], nums2 = [2, 4, 5]: output = 4", () => {
     // 1,2,3,5,7,9,11
-    expect(findMedianSortedArrays([1,3,7,9], [2, 4, 5, 11])).to.be.eq(4);
+    expect(findMedianSortedArrays([1,3,7,9], [2, 4, 5])).to.be.eq(4.);
   });
 
   it("nums1 = [1,2,3,4], nums2 = [5, 6, 7]: output = 4", () => {
@@ -20,7 +20,34 @@ describe("median-two-sorted-arrays", () => {
     expect(findMedianSortedArrays([1,2,3,7,8,9], [4, 5, 6])).to.be.eq(5);
   });
   it("nums1 = [1,2], nums2 = [3, 4]: output = 2.5", () => {
-    // 1,2,3,5,7,9,11
-    expect(findMedianSortedArrays([1,2], [3, 4])).to.be.eq(5);
+    expect(findMedianSortedArrays([1,2], [3, 4])).to.be.eq(2.5);
+  });
+  it("nums1 = [1], nums2 = []: output = 1", () => {
+    expect(findMedianSortedArrays([1], [])).to.be.eq(1);
+  });
+  it("nums1 = [], nums2 = [1]: output = 1", () => {
+    expect(findMedianSortedArrays([], [1])).to.be.eq(1);
+  });
+  it("nums1 = [1,3], nums2 = [2,7]: output = 1", () => {
+    expect(findMedianSortedArrays([1,3], [2,7])).to.be.eq(2.5);
+  });
+
+  it("nums1 = [1,3], nums2 = []: output = 2", () => {
+    expect(findMedianSortedArrays([1,3], [])).to.be.eq(2);
+  });
+  it("nums1 = [2,3,4,5], nums2 = []: output = 3.5", () => {
+    expect(findMedianSortedArrays([2,3,4,5], [])).to.be.eq(3.5);
+  });
+  it("nums1 = [1], nums2 = [2,3]: output = 2", () => {
+    expect(findMedianSortedArrays([1], [2,3])).to.be.eq(2);
+  });
+  it("nums1 = [2,3,4], nums2 = [1]: output = 2.5", () => {
+    expect(findMedianSortedArrays([2,3,4], [1])).to.be.eq(2.5);
+  });
+  it("nums1 = [0,0], nums2 = [0,0]: output = 0", () => {
+    expect(findMedianSortedArrays([0,0], [0, 0])).to.be.eq(0);
+  });
+  it("nums1 = [], nums2 = [1,2,3,4]: output = 2.5", () => {
+    expect(findMedianSortedArrays([], [1,2,3,4])).to.be.eq(2.5);
   });
 });
