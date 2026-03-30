34caff0f9d5ba8df8d648ab052a23462d140f110
akoidan <deathangel908@gmail.com>
2026-03-31 01:45:52 +0200

    patterns

---
+++ java/src/LOL.java
@@ -0,0 +1,25 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+
+
+public class LOL {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void treeError() {
+        Set<Number> set = new TreeSet<>();
+        set.add(1);
+        set.add(1L);
+        set.add(1.0);
+    }
+
+    void run(){
+        System.out.println("asd");
+        http://localhost
+        return;
+    }
+}
