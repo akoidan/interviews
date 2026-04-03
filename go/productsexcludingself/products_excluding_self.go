bcae603ea7bce95d2f186f7a8b58b1671300b809
akoidan <deathangel908@gmail.com>
2026-04-04 01:53:02 +0200

    nebius

--- go/productsexcludingself/products_excluding_self.go
+++ go/productsexcludingself/products_excluding_self.go
@@ -3,6 +3,25 @@ package productsexcludingself
 // https://neetcode.io/problems/products-of-array-discluding-self/question
 
 func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	res[len(nums)-1] = 1
+	// suffix (right to the element multiplier for each element
+	// also use result the same way as prefix array
+	for i := len(nums) - 2; i >= 0; i-- {
+		res[i] = res[i+1] * nums[i+1]
+	}
+	// prev: = prefix, but since we can iterate within the same cycle as generate prefix
+	// we just remember the last element, that's it
+	prev := 1
+	for i := 0; i < len(nums); i++ {
+		res[i] = prev * res[i]
+		prev = nums[i] * prev
+	}
+
+	return res
+}
+
+func productExceptSelfMine(nums []int) []int {
 	prefix := make([]int, len(nums))
 	suffix := make([]int, len(nums))
 	prefix[0] = 1
