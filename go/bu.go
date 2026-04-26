54b8f2eaa474d50f9ffadee69b50731758198bc8
Andrew Koidan <deathangel908@gmail.com>
2026-04-27 00:17:30 +0200

    Update bu.go

--- go/bu.go
+++ go/bu.go
@@ -39,32 +39,41 @@ func findWords(board [][]byte, words []string) []string {
 	}
 	fmt.Printf("\n\n\n")
 
-	b, _ := json.MarshalIndent(root, "", "  ")
-	fmt.Println("???")
-	fmt.Println(string(b))
-	fmt.Println("???")
-
-	dfs := func(point Point, visited map[Point]bool, node Node, s string) {
+	// b, _ := json.MarshalIndent(root, "", "  ")
+	// fmt.Println("???")
+	// fmt.Println(string(b))
+	// fmt.Println("???")
+	var dfs func(point Point, visited map[Point]bool, node Node, s string) 
+	dfs = func(point Point, visited map[Point]bool, node Node, s string) {
 		if visited[point] || point.x < 0 || point.y < 0 || point.x >= width || point.y >= height {
+			fmt.Printf("%s%v!", s, point)
 			return
 		}
 		visited[point] = true
 		char := rune(board[point.y][point.x])
 		nextNode, ok := node.Children[char]
+		fmt.Printf("===%v===", char)
 		if !ok {
+			fmt.Printf("%s%v|",s, point)
 			return
 		}
+		fmt.Printf("%s%v\n", s,point)	
 		if nextNode.IsEnd {
-			exists[s] = true
+			exists[s+string(char)] = true
 		}
 		
 		dfs(Point{point.y-1, point.x}, cloneMap(visited), nextNode, s + string(char))
 		dfs(Point{point.y, point.x-1}, cloneMap(visited), nextNode, s + string(char))
 		dfs(Point{point.y+1, point.x}, cloneMap(visited), nextNode, s + string(char))
-		dfs(Point{point.y+1, point.x}, cloneMap(visited), nextNode, s + string(char))
+		dfs(Point{point.y, point.x+1}, cloneMap(visited), nextNode, s + string(char))
 	}
 
-	dfs(Point{0,0}, make(map[Point]bool), root, "") 
+	for y := 0; y< height; y++ {
+		for x := 0; x< width; x++ {
+			dfs(Point{y,x}, make(map[Point]bool), root, "") 
+			fmt.Printf("\n\n")
+		}
+	}
 	res := []string {}
 	for k, _ := range exists {
 		res = append(res, k)
@@ -74,7 +83,7 @@ func findWords(board [][]byte, words []string) []string {
 
 func cloneMap(in map[Point]bool) map[Point]bool {
 	res := map[Point]bool{}
-	for k,v := range visited {
+	for k,v := range in {
 		res[k] = v
 	}
 	return res
